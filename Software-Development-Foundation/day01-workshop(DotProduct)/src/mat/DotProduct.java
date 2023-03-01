b60aac98979e80ded3d99189b809aa8b0fd8694c
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-01 23:21:21 +0800

    added day01-workshop(DotProduct)

---
+++ Software-Development-Foundation/day01-workshop(DotProduct)/src/mat/DotProduct.java
@@ -0,0 +1,51 @@
+package mat;
+
+import java.io.Console;
+
+public class DotProduct {
+    public static void main(String[] args) {
+        
+        int rows = Integer.parseInt(args[0]);
+        int cols = Integer.parseInt(args[1]);
+
+        // array for the data
+        int[][] data = new int[rows][cols];
+        // array for the weights
+        int[] weights = new int[cols];
+        // array for the result of the dot product
+        int[] result = new int[rows];
+
+        Console cons = System.console();
+        String input;
+        String[] values;
+
+        // Read the data
+        for (int i = 0; i < rows; i++) {
+            input = cons.readLine("Row %d, %d values: ", i, cols);
+            values = input.trim().split(" ");
+            for (int j = 0; j < cols; j++)
+                data[i][j] = Integer.parseInt(values[j]);
+        }
+
+        // read the weights
+        input = cons.readLine("Weights: %d: ", cols);
+        values = input.trim().split(" ");
+        for (int i = 0; i < cols; i++) {
+            weights[i] = Integer.parseInt(values[i]);
+        }
+
+        // perfrom the dot product
+        for (int i = 0; i < rows; i++) {
+            int total = 0;
+            for (int j = 0; j < cols; j++) {
+                total = total + (data[i][j] * weights[j]);
+            }
+            result[i] = total;
+        }
+
+        // print the result of teh dot product
+        for (int i = 0; i < rows; i++) {
+            System.out.printf(">>>> %d\n", result[i]);
+        }
+    }
+}
