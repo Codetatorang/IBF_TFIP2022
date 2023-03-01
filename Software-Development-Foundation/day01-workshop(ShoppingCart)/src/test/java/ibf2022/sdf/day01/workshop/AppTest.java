342369360b6f42a75e78f0c34ae1a2964cd6c7f6
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-01 23:34:06 +0800

    initial commit

---
+++ Software-Development-Foundation/day01-workshop(ShoppingCart)/src/test/java/ibf2022/sdf/day01/workshop/AppTest.java
@@ -0,0 +1,18 @@
+package ibf2022.sdf.day01.workshop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
