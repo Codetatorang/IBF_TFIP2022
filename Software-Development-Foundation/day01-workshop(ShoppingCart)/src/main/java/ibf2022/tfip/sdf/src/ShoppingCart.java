32b4fecbcb573182f1d5fe65c4e7ebe50487b4f6
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-07 01:50:51 +0800

    completed day01-workshop-shoppingcart

---
+++ Software-Development-Foundation/day01-workshop(ShoppingCart)/src/main/java/ibf2022/tfip/sdf/src/ShoppingCart.java
@@ -0,0 +1,77 @@
+package ibf2022.tfip.sdf.src;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    public static void main(String[] args) {
+        List<String> cartItems = new ArrayList<String>();
+
+        // message
+        System.out.println("Welcome to your shopping cart");
+
+        Console cons = System.console();
+        // boolean to check if exited
+        Boolean exit = false;
+
+        while (!exit) {
+            // reads input and store as a string
+            String inputString = cons.readLine(">");
+            // transform input into array
+            String inputArr[] = inputString.toLowerCase().trim().split(" ");
+
+            switch (inputArr[0]) {
+                case "list":
+                    if (cartItems.isEmpty()) {
+                        System.out.println("Your cart is empty");
+                        break;
+                    }
+
+                    for (int i = 0; i < cartItems.size(); i++) {
+                        System.out.printf("%d. %s\n", i + 1, cartItems.get(i));
+                    }
+                    break;
+                case "add":
+                    if (inputArr.length < 2) {
+                        System.out.println("Please specify something to add");
+                        continue;
+                    }
+                    for (int i = 1; i < inputArr.length; i++) {
+                        if (cartItems.size() > 0 && cartItems.contains(inputArr[i])) {
+                            System.out.printf("You have %s in your cart\n", inputArr[i]);
+                            continue;
+                        }
+                        cartItems.add(inputArr[i]);
+                        System.out.printf("%s added to cart\n", inputArr[i]);
+                    }
+                    break;
+                case "delete":
+                    if (inputArr.length < 2) {
+                        System.out.println("Please specify an index to delete\n");
+                        continue;
+                    }
+                    try{
+                        Integer index = Integer.parseInt(inputArr[1]);
+                        if (index > cartItems.size()) {
+                            System.out.println("Number not in list\n");
+                            break;
+                        }
+                        System.out.printf("%s removed from cart\n", cartItems.get(index - 1));
+                        cartItems.remove(index - 1);
+                    }catch(NumberFormatException ex){
+                        System.out.println("Please enter the index instead of the word");
+                        continue;
+                    }
+                    break;
+                case "exit":
+                    System.out.println("exiting from programme");
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Please enter either 'list', 'add' , 'delete' or 'exit");
+            }
+        }
+
+    }
+}
