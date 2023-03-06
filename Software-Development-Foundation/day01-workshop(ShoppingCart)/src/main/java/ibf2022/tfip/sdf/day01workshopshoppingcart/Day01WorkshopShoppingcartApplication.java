7ab0bc249a9bb29954df4e3c4bfccce965025345
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-06 14:16:48 +0800

    inital commit day01-workshop-shoppingcart

---
+++ Software-Development-Foundation/day01-workshop(ShoppingCart)/src/main/java/ibf2022/tfip/sdf/day01workshopshoppingcart/Day01WorkshopShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.sdf.day01workshopshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day01WorkshopShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day01WorkshopShoppingcartApplication.class, args);
+	}
+
+}
---
+++ Software-Development-Foundation/day01-workshop(ShoppingCart)/src/test/java/ibf2022/tfip/sdf/day01workshopshoppingcart/Day01WorkshopShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.sdf.day01workshopshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day01WorkshopShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
