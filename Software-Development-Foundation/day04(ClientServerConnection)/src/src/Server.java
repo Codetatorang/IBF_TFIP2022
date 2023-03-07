331452ed4771149eea5ed575245b8960c823167a
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-08 02:09:56 +0800

    completed day04(ClientServerConnection)

---
+++ Software-Development-Foundation/day04(ClientServerConnection)/src/src/Server.java
@@ -0,0 +1,56 @@
+package src;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[]args) throws IOException{
+    System.out.println("Starting server on port 3000");
+    //Create a server socket and listen to a specific port
+    ServerSocket server = new ServerSocket(3000);
+    
+    while(true){
+            //wait for a connection
+            System.out.println("Waiting incoming connection");
+            Socket conn = server.accept();
+            
+            System.out.println("Connection connected.");
+            //Do something
+            //Get the input stream, read the data from the client
+            InputStream is = conn.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            // Reader r = new InputStreamReader(is);
+            // BufferedReader br = new BufferedReader(r);
+
+            // String input = br.readLine();
+            String input = ois.readUTF();
+            // System.out.printf(">> from client: %s\n", input);
+            System.out.printf(">> from client: %s\n", input);
+            input = input.toUpperCase();
+            System.out.printf(">> from client: %s\n", input);
+
+            OutputStream os = conn.getOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            
+            // input = input.toUpperCase();
+            // OutputStream os = conn.getOutputStream();
+            // Writer w = new OutputStreamWriter(os);
+            // w.write(input);
+            // w.flush();
+            
+            //Close the connection
+            conn.close();
+            server.close();
+            oos.close();
+
+        }
+    
+
+    }
+
+}
