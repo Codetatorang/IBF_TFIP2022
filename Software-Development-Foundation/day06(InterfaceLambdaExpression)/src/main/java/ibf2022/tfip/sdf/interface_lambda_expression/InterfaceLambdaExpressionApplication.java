f0926a5a86a27ba307fb7a82d167d5d81d1f1cc6
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-12 23:16:41 +0800

    clean workspace, inital commit

---
+++ Software-Development-Foundation/day06(InterfaceLambdaExpression)/src/main/java/ibf2022/tfip/sdf/interface_lambda_expression/InterfaceLambdaExpressionApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.sdf.interface_lambda_expression;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceLambdaExpressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceLambdaExpressionApplication.class, args);
+	}
+
+}
---
+++ Software-Development-Foundation/day06(InterfaceLambdaExpression)/src/test/java/ibf2022/tfip/sdf/interface_lambda_expression/InterfaceLambdaExpressionApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.sdf.interface_lambda_expression;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceLambdaExpressionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
