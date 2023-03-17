c749d9c533f597a0bd657765dc69ee4fffd18ab2
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-17 14:27:25 +0800

    initial commit

---
+++ Software-Development-Foundation/day08(ReadWriteRevision)/src/src/ArraySortExample.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraySortExample {
+    public void example(){
+        //custom array with daat (name)
+        String name[] = {"daryl","elaine","james","zoanne","chris","mark","sophia","asher","brandon","helen","daniel","marcus"};
+
+        //print out unsorted array
+        System.out.println("unsorted arr: " + Arrays.toString(name));
+
+        //sort the array in ascending order
+        Arrays.sort(name);
+
+        //print out sorted array
+        System.out.println("Sorted arr: " +Arrays.toString(name));
+
+        //sort array in descending order
+        Arrays.sort(name,Collections.reverseOrder());
+    }
+}
