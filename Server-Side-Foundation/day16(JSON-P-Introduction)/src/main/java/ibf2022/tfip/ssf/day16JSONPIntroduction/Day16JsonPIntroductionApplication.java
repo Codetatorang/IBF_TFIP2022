79d610f82dd3f577b02336bb6396d867ad771747
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-03-24 21:31:58 +0800

    initial commit

---
+++ Server-Side-Foundation/day16(JSON-P-Introduction)/src/main/java/ibf2022/tfip/ssf/day16JSONPIntroduction/Day16JsonPIntroductionApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.ssf.day16JSONPIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day16JsonPIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day16JsonPIntroductionApplication.class, args);
+	}
+
+}
