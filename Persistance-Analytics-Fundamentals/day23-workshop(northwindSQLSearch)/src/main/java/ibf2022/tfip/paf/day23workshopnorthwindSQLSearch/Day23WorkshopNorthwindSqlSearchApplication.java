166a236ac526b4e3f0e1eeb92c078a38c8d54146
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-05-10 12:44:33 +0800

    initial commit

---
+++ Persistance-Analytics-Fundamentals/day23-workshop(northwindSQLSearch)/src/main/java/ibf2022/tfip/paf/day23workshopnorthwindSQLSearch/Day23WorkshopNorthwindSqlSearchApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.paf.day23workshopnorthwindSQLSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day23WorkshopNorthwindSqlSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day23WorkshopNorthwindSqlSearchApplication.class, args);
+	}
+
+}
---
+++ Persistance-Analytics-Fundamentals/day23-workshop(northwindSQLSearch)/src/test/java/ibf2022/tfip/paf/day23workshopnorthwindSQLSearch/Day23WorkshopNorthwindSqlSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.paf.day23workshopnorthwindSQLSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day23WorkshopNorthwindSqlSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
