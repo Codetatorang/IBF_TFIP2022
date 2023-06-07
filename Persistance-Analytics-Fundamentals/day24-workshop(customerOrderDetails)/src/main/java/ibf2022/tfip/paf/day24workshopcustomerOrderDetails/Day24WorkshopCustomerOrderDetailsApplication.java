355bb6380d5d1bdaa01bd725776f64516876c595
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2023-06-07 13:56:05 +0800

    initial commit

---
+++ Persistance-Analytics-Fundamentals/day24-workshop(customerOrderDetails)/src/main/java/ibf2022/tfip/paf/day24workshopcustomerOrderDetails/Day24WorkshopCustomerOrderDetailsApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.paf.day24workshopcustomerOrderDetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day24WorkshopCustomerOrderDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day24WorkshopCustomerOrderDetailsApplication.class, args);
+	}
+
+}
---
+++ Persistance-Analytics-Fundamentals/day24-workshop(customerOrderDetails)/src/test/java/ibf2022/tfip/paf/day24workshopcustomerOrderDetails/Day24WorkshopCustomerOrderDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.paf.day24workshopcustomerOrderDetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day24WorkshopCustomerOrderDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
