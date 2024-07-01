0fde988bf1f8287ec2cd270d1913db1c18084a9f
Vincent Ang <102163869+Codetatorang@users.noreply.github.com>
2024-07-01 20:29:58 +0800

    uncommented codes and clean up

--- Server-Side-Foundation/day16(JSON-P-Introduction)/src/main/java/ibf2022/tfip/ssf/day16JSONPIntroduction/Day16JsonPIntroductionApplication.java
+++ Server-Side-Foundation/day16(JSON-P-Introduction)/src/main/java/ibf2022/tfip/ssf/day16JSONPIntroduction/Day16JsonPIntroductionApplication.java
@@ -2,44 +2,45 @@ package ibf2022.tfip.ssf.day16JSONPIntroduction;
 
 /* import java.io.Reader;
 import java.io.StringReader; */
+import java.io.Reader;
+import java.io.StringReader;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/* import jakarta.json.Json;
+import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
-import jakarta.json.JsonReader; */
+import jakarta.json.JsonReader;
 
 @SpringBootApplication
 public class Day16JsonPIntroductionApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day16JsonPIntroductionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Day16JsonPIntroductionApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) {
+    @Override
+    public void run(String... args) {
 
-		/* 
-		// json-p object creation with json object builder
-		JsonObject jsonObj = Json.createObjectBuilder()
-				.add("name", "fred")
-				.add("email", "fred@gmail.com")
-				.build();
-		System.out.printf(">>>> jsonObject: %s\n", jsonObj.toString());
+        // json-p object creation with json object builder
+        JsonObject jsonObj = Json.createObjectBuilder()
+                .add("name", "fred")
+                .add("email", "fred@gmail.com")
+                .build();
+        System.out.printf(">>>> jsonObject: %s\n", jsonObj.toString());
 
-		// json-p array creation with json array builder
-		JsonArray jsonArray = Json.createArrayBuilder()
-				.add(jsonObj)
-				.add(jsonObj)
-				.add(jsonObj)
-				.build();
-		System.out.printf(">>>> jsonArray: %s\n", jsonArray.toString());
+        // json-p array creation with json array builder
+        JsonArray jsonArray = Json.createArrayBuilder()
+                .add(jsonObj)
+                .add(jsonObj)
+                .add(jsonObj)
+                .build();
+        System.out.printf(">>>> jsonArray: %s\n", jsonArray.toString());
 
-		// create a json format string then convert to json object
-		String jsonStr = """
+        // create a json format string then convert to json object
+        String jsonStr = """
 					{
 						"orderId": 1234, "address": "10 bedrock ave",
 						"lineItems":[
@@ -50,25 +51,24 @@ public class Day16JsonPIntroductionApplication implements CommandLineRunner {
 
 				""";
 
-		System.out.printf(">>>> jsonStr: %s\n", jsonStr);
+        System.out.printf(">>>> jsonStr: %s\n", jsonStr);
 
-		//converts string json to json using json reader
-		Reader reader = new StringReader(jsonStr);
-		JsonReader jsonReader = Json.createReader(reader);
-		jsonObj = jsonReader.readObject();
+        //converts string json to json using json reader
+        Reader reader = new StringReader(jsonStr);
+        JsonReader jsonReader = Json.createReader(reader);
+        jsonObj = jsonReader.readObject();
 
-		System.out.printf("order:Id: %d\n", jsonObj.getInt("orderId"));
-		System.out.printf("address:Id: %s\n", jsonObj.getString("address"));
+        System.out.printf("order:Id: %d\n", jsonObj.getInt("orderId"));
+        System.out.printf("address:Id: %s\n", jsonObj.getString("address"));
 
-		//retrieve the array values in the lineItems key
-		jsonArray = jsonObj.getJsonArray("lineItems");
+        //retrieve the array values in the lineItems key
+        jsonArray = jsonObj.getJsonArray("lineItems");
 
-		for (int i = 0; i < jsonArray.size(); i++) {
-		jsonObj = jsonArray.getJsonObject(i);
-		System.out.printf("\tname: %s, quantity: %d\n", jsonObj.getString("name"),
-		jsonObj.getInt("quantity"));
-		}
-		
-		*/
-	}
+        for (int i = 0; i < jsonArray.size(); i++) {
+            jsonObj = jsonArray.getJsonObject(i);
+            System.out.printf("\tname: %s, quantity: %d\n", jsonObj.getString("name"),
+                    jsonObj.getInt("quantity"));
+        }
+
+    }
 }
